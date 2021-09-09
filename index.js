88bb93fdfb80a220bc7171ef14b57408678affe9
Gregory Presser <gpress2222@gmail.com>
2021-09-09 17:39:01 -0400

    fixed some sign in issues and added removing stuff

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+const oldMovies = require("./movies.json");
 const user = "gip2000";
+
 
 (async() => {
     const browser = await puppeteer.launch({ userDataDir: __dirname + "/userData" });
     const page = await browser.newPage();
-    const watchlistMovie = async(str) => {
+    const watchlistMovie = async(str, inverse = false) => {
+        console.log(`movie ${str}. inverse = ${inverse}`);
         await page.goto("https://www.google.com/search?q=" + str, { waitUntil: 'networkidle2' });
-        const bool = await page.evaluate(() => {
+        let isOnWatchlist = await page.evaluate(() => {
             return document.getElementById("NXD9g").classList.length === 2;
         });
-        if (!bool) return;
+        if (inverse) isOnWatchlist = !isOnWatchlist;
+        if (!isOnWatchlist) return;
         await page.click("#NXD9g");
     }
     await page.goto(`https://letterboxd.com/${user}/watchlist/`, { waitUntil: 'networkidle2' });
@@ -19,6 +24,12 @@ const user = "gip2000";
     const movies = data.split('\n').map(movie => movie.split(",")[1] + " " + movie.split(",")[2]).slice(1, -1);
     for (let i = 0; i < movies.length; i++)
         await watchlistMovie(movies[i]);
+    for (let i = 0; i < oldMovies.length; i++) {
+        if (movies.includes(oldMovies[i])) continue;
+        await watchlistMovie(oldMovies[i], true);
+    }
+
+    fs.writeFileSync('./movies.json', JSON.stringify(movies));
     await browser.close();
 
 })();
--- signin.js
+++ signin.js
@@ -1,14 +1,74 @@
 const puppeteer = require('puppeteer-extra');
 const pluginStealth = require('puppeteer-extra-plugin-stealth');
+const { letterBoxd, google } = require('./accountInfo.json')
 puppeteer.use(pluginStealth());
-const user = "gip2000";
+
+const checkExists = async(selector, page) => {
+    const elementHandle = await page.$(selector);
+    return elementHandle !== null;
+}
+
+const checkIfSignIn = async(selector, page) => {
+    return page.evaluate((selector) => {
+        const DOM = document.querySelector(selector);
+        return DOM?.innerText?.includes("SIGN IN");
+    }, selector);
+    
+}
+
+
+const googleSignIn = async(page = null) => {
+    let browser;
+    if (page == null) {
+        browser = await puppeteer.launch({
+            userDataDir: __dirname + "/userData",
+            headless: false
+        });
+        page = await browser.newPage();
+    }
+
+    await page.goto("https://accounts.google.com");
+    const exists = await checkExists('input[type=email]', page);
+    if (!exists) return;
+    await page.type('input[type=email]', google.email);
+    await page.click(".VfPpkd-RLmnJb");
+    await page.waitForSelector("input[type=password]", { visible: true, timeout: 0 });
+    await page.type('input[type=password]', google.password);
+    await page.click(".VfPpkd-RLmnJb");
+    await page.waitForSelector(".B1tEqd",{ visible: true, timeout: 0 })
+    if (browser != null)
+        await browser.close();
+}
+
+const letterBoxdSignIn = async(page = null) => {
+        let browser = null;
+        if (page == null) {
+            browser = await puppeteer.launch({
+                userDataDir: __dirname + "/userData",
+                headless: false
+            });
+            page = await browser.newPage();
+        }
+        await page.goto(`https://letterboxd.com/${letterBoxd.user}/watchlist/`);
+        if(!(await checkIfSignIn("a.navlink.has-icon", page))) return;
+        await page.click("a.navlink.has-icon");
+        await page.type("input#username[type=email]", letterBoxd.email);
+        await page.type("input#password[type=password]", letterBoxd.password);
+        await page.click("input#remember[type=checkbox]");
+        await page.click("input.button.-action.button-green[type=submit]");
+        await page.waitForSelector("a.has-icon.toggle-menu", { visible: true, timeout: 0 });
+        
+        if (browser != null)
+            await browser.close();
+
+    }
 (async() => {
     const browser = await puppeteer.launch({
         userDataDir: __dirname + "/userData",
         headless: false
     });
     const page = await browser.newPage();
-    await page.goto("https://accounts.google.com");
-    const page2 = await browser.newPage();
-    await page2.goto(`https://letterboxd.com/${user}/watchlist/`);
+    await googleSignIn(page);
+    await letterBoxdSignIn(page);
+    await browser.close();
 })();
